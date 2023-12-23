9e7a1d945671390a272d56415e883c2dfc6bf9b6
FenyxInvincible <e.rebrakov@gmail.com>
2023-12-23 03:43:38 +0200

    Update LockIK.py
    Added several buttons and actions like adjusting chain length

--- LockIK.py
+++ LockIK.py
@@ -59,67 +59,76 @@ class LockSelectedBonesOperator(bpy.types.Operator):
                 helper_bone.tail = bone.tail + mathutils.Vector((0, 0.2, 0))  # Give the bone some length
                 helper_bone.parent = None  # The helper bone is not connected to the parent
                 helper_bone_name = helper_bone.name
-
-                # Switch back to pose mode to add the IK constraint
+    
+                # Switch back to pose mode for setting the IK constraint
                 bpy.ops.object.mode_set(mode='POSE')
-
+    
                 # Add an IK constraint to the selected bone with the helper bone as the target
                 helper_pose_bone = armature.pose.bones[helper_bone_name]
                 helper_pose_bone.custom_shape = shape_cube
-
+    
                 constraint = bone.constraints.new('IK')
                 constraint.name = 'LockIK'
                 constraint.target = saved_armature
                 constraint.subtarget = helper_bone_name
-
+    
                 # If settings exist in the ik_settings dictionary, use them
                 if bone.name in ik_settings:
                     constraint.iterations = ik_settings[bone.name]['iterations']
                     constraint.chain_count = ik_settings[bone.name]['chain_count']
                     constraint.use_tail = ik_settings[bone.name]['use_tail']
-
+    
                 # Otherwise, calculate chain length based on connected parent bones
                 else:
                     parent_bone = bone.parent
-                    chain_length = 1 
+                    chain_length = 1
                     while parent_bone and parent_bone.bone.use_connect:
                         chain_length += 1
                         parent_bone = parent_bone.parent
                     constraint.chain_count = chain_length + 1  # Include the bone itself in the chain length
+
         return {'FINISHED'}
 
 class InsertKeyframesOperator(bpy.types.Operator):
     bl_idname = "object.insert_keyframes_operator"
     bl_label = "Insert Keyframes"
     bl_description = " Doesn't clear IK lock. Inserts a keyframe for every bone's location and rotation, not only for the selected ones."
-    
+
     @classmethod
     def poll(cls, context):
         return context.mode == 'POSE'
 
     def execute(self, context):
-        
+
+        # Remember if a single helper bone is selected
+        selected_helper_bone_name = None
+        if is_helper_bone_selected(context):
+            selected_helper_bone_name = context.selected_pose_bones[0].name
+
         # Execute the bake operator
         bpy.ops.object.bake_operator()
-        
+
+        armature = bpy.context.object
+
         # Unselect all bones
         bpy.ops.pose.select_all(action='DESELECT')
         
-        # Select bones previously stored in the global list
-        armature = bpy.context.object
-        
-        for bone_name in bones_with_ik: 
-            print(bone_name)
-        
         for bone_name in bones_with_ik:
-            bone = armature.data.bones.get(bone_name) # replace "LeftFoot" by your `bone_name`
+            bone = armature.data.bones.get(bone_name)
             if bone:
                 bone.select = True
-                armature.data.bones.active = bone            
-
+                armature.data.bones.active = bone
+    
         # Execute the Lock Selected Bones operator on the selected bones
         bpy.ops.object.lock_selected_bones_operator()
 
+        # If a single helper bone was selected, select it again, otherwise select bones in the bones_with_ik list
+        if selected_helper_bone_name is not None:
+            helper_bone = armature.data.bones.get(selected_helper_bone_name)
+            if helper_bone:
+                bpy.ops.pose.select_all(action='DESELECT')
+                helper_bone.select = True
+                armature.data.bones.active = helper_bone
         return {'FINISHED'}
         
 def save_bone_transforms(bone):
@@ -150,6 +159,75 @@ def remove_ik_constraints(bone):
             
     for child in bone.children:
         remove_ik_constraints(child)
+
+def is_helper_bone_selected(context):
+    return (len(context.selected_pose_bones) == 1 and
+            (context.selected_pose_bones[0].name.endswith('_Pole_LockIKTarget') or
+             context.selected_pose_bones[0].name.endswith('_LockIKTarget')))
+
+def get_original_bone(context):
+    if len(context.selected_pose_bones) != 1:
+        return False
+    bone = context.selected_pose_bones[0]
+    if bone.name.endswith('_Pole_LockIKTarget'):
+        return context.object.pose.bones[bone.name.replace('_Pole_LockIKTarget', '')]
+    elif bone.name.endswith('_LockIKTarget'):
+        return context.object.pose.bones[bone.name.replace('_LockIKTarget', '')]
+    elif 'LockIK' in (c.name for c in bone.constraints):
+        return bone
+    else:
+        return False
+
+class AddPoleTargetOperator(bpy.types.Operator):
+    bl_idname = "object.add_pole_target_operator"
+    bl_label = "Add Pole Target"
+    bl_description = "Adds a pole target for the selected bones"
+
+    @classmethod
+    def poll(cls, context):
+        if context.mode != 'POSE':
+            return False
+
+        # Check if all selected pose bones have a LockIK constraint
+        armature = context.object
+        for bone in context.selected_pose_bones:
+            if 'LockIK' not in (c.name for c in bone.constraints):
+                return False
+
+        return True
+
+    def execute(self, context):
+        armature = context.object
+
+        for bone in context.selected_pose_bones:
+            # Assuming 'LockIK' is the name of the IK constraint
+            constraint = bone.constraints['LockIK']
+
+            # Create the pole target bone at the head location
+            bpy.ops.object.mode_set(mode='EDIT')
+            pole_helper_bone = armature.data.edit_bones.new(bone.name + "_Pole_LockIKTarget")
+            pole_helper_bone.head = bone.head
+            pole_helper_bone.tail = bone.head + mathutils.Vector((0, 0.1, 0))  # Should be adjusted according to your requirements
+            pole_helper_bone.parent = None  # The helper bone is not connected to the parent
+            pole_helper_bone_name = pole_helper_bone.name
+
+            bpy.ops.object.mode_set(mode='POSE')
+
+            # Set helper_bone custom shape to shape_cube 
+            pole_helper_pose_bone = armature.pose.bones[pole_helper_bone_name]
+
+            # Add custom shape
+            shape_cube = bpy.data.objects.get('LockIKTargetDisplay')
+            if shape_cube is not None:  # Make sure the cube exists
+                pole_helper_pose_bone.custom_shape = shape_cube
+
+            # Set the created helper bone as the pole target
+            constraint.pole_target = armature
+            constraint.pole_subtarget = pole_helper_bone_name
+
+            # Compute the pole angle (if needed) and assign to the constraint here
+
+        return {'FINISHED'}
 
 class BakeOperator(bpy.types.Operator):
     bl_idname = "object.bake_operator"
@@ -196,6 +274,73 @@ class BakeOperator(bpy.types.Operator):
         
         return {'FINISHED'}
 
+class AdjustChainLengthOperator(bpy.types.Operator):
+    bl_idname = "object.adjust_chain_length_operator"
+    bl_label = "Adjust IK Chain Length"
+    bl_description = "Increases or decreases the pole length depending on the mouse scroll direction."
+    direction: bpy.props.IntProperty()  # still need this to store the scroll direction
+
+    @classmethod
+    def poll(cls, context):
+        return context.mode == 'POSE' and get_original_bone(context) is not False
+
+    def execute(self, context):
+        armature = bpy.context.object
+        original_bone = get_original_bone(context)
+        if not original_bone:
+            return {'CANCELLED'}
+
+        # Check if the bone's name is in the ik_settings
+        if original_bone.name not in ik_settings:
+            ik_settings[original_bone.name] = {
+                'iterations': None,
+                'chain_count': None,
+                'use_tail': None
+            }
+
+        # Find the IK constraint and adjust its pole length
+        for constraint in original_bone.constraints:
+            if constraint.type == 'IK':
+                constraint.chain_count = max(0, constraint.chain_count + self.direction)
+                ik_settings[original_bone.name]['chain_count'] = constraint.chain_count
+                # Show a message with the adjusted chain length
+                self.report({'INFO'}, f"Adjusted length to {constraint.chain_count}")
+        return {'FINISHED'}
+
+class RemoveIKOperator(bpy.types.Operator):
+    bl_idname = "object.remove_ik_operator"
+    bl_label = "Remove IK"
+    bl_description = "Remove an IK constraint, and delete helper bone if it exists."
+
+    @classmethod
+    def poll(cls, context):
+        return context.mode == 'POSE' and get_original_bone(context) is not False
+
+    def execute(self, context):
+        original_bone = get_original_bone(context)
+        if not original_bone:
+            self.report({'ERROR'}, "No valid bone to remove IK from.")
+            return {'CANCELLED'}
+
+        # Remove the 'LockIK' constraint from the original bone, if exists
+        for constraint in original_bone.constraints:
+            if constraint.type == 'IK' and 'LockIK' in constraint.name:
+                original_bone.constraints.remove(constraint)
+
+        # Delete the helper bones, if they exist
+        remove_bone_names = [original_bone.name + suffix for suffix in ['_LockIKTarget', '_Pole_LockIKTarget']]
+        remove_bones = [context.object.pose.bones.get(bone_name) for bone_name in remove_bone_names]
+        if any(remove_bones):
+            bpy.ops.object.mode_set(mode='EDIT')
+            for remove_bone_name in remove_bone_names:
+                edit_bone = context.object.data.edit_bones.get(remove_bone_name)  # Get the EditBone
+                if edit_bone:
+                    context.object.data.edit_bones.remove(edit_bone)
+            bpy.ops.object.mode_set(mode='POSE')
+
+        self.report({'INFO'}, f"Removed IK from {original_bone.name}")
+        return {'FINISHED'}
+
 class LockIK_Panel(bpy.types.Panel):
     bl_label = "LockIK"
     bl_idname = "OBJECT_PT_ikh_handler"
@@ -210,20 +355,52 @@ class LockIK_Panel(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         layout.operator("object.lock_selected_bones_operator")
+        layout.operator("object.add_pole_target_operator")
+        if RemoveIKOperator.poll(context) != False:
+            layout.operator("object.remove_ik_operator")
+        
         layout.operator("object.insert_keyframes_operator")
         layout.operator("object.bake_operator")
+        # Draw label if AdjustPoleLengthOperator can be executed
+        if AdjustChainLengthOperator.poll(context) != False:
+            layout.label(text="Press Ctrl + Mouse Wheel to adjust chain length")
 
 def register():
     bpy.utils.register_class(LockSelectedBonesOperator)
     bpy.utils.register_class(BakeOperator)
     bpy.utils.register_class(InsertKeyframesOperator)
     bpy.utils.register_class(LockIK_Panel)
+    bpy.utils.register_class(AddPoleTargetOperator)
+    bpy.utils.register_class(AdjustChainLengthOperator)
+    bpy.utils.register_class(RemoveIKOperator)
+
+    # create keymap items
+    kc = bpy.context.window_manager.keyconfigs.addon
+    if kc:
+        km = kc.keymaps.new(name='3D View', space_type='VIEW_3D')
+        kmi = km.keymap_items.new(AdjustChainLengthOperator.bl_idname, 'WHEELUPMOUSE', 'PRESS', ctrl=True)
+        kmi.properties.direction = 1
+        
+        kmi = km.keymap_items.new(AdjustChainLengthOperator.bl_idname, 'WHEELDOWNMOUSE', 'PRESS', ctrl=True)
+        kmi.properties.direction = -1
+
+        km.keymap_items.new(InsertKeyframesOperator.bl_idname, 'MIDDLEMOUSE', 'PRESS', ctrl=True)
 
 def unregister():
     bpy.utils.unregister_class(LockSelectedBonesOperator)
     bpy.utils.unregister_class(BakeOperator)
     bpy.utils.unregister_class(InsertKeyframesOperator)
     bpy.utils.unregister_class(LockIK_Panel)
+    bpy.utils.unregister_class(AddPoleTargetOperator)
+    bpy.utils.unregister_class(AdjustChainLengthOperator)
+    bpy.utils.unregister_class(RemoveIKOperator)
+
+    # remove keymap items
+    kc = bpy.context.window_manager.keyconfigs.addon
+    if kc:
+        for kmi in km.keymap_items:
+            if kmi.idname in ["object.adjust_chain_length_operator","object.insert_keyframes_operator"]:
+                km.keymap_items.remove(kmi)
 
 if __name__ == "__main__":
     register()
